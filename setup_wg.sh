f8478c408f5b9e0d04b4c88b0cf025aa4b91e21b
middtho-dev <middtho@yandex.ru>
2025-03-12 22:54:56 +0300

    Update setup_wg.sh

--- setup_wg.sh
+++ setup_wg.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-# Функция для вывода сообщений в зелёном цвете
-log_green() {
-    echo -e "\033[32m$1\033[0m"
-}
-
-# 1. Установка WireGuard
-log_green "Проверка наличия WireGuard..."
+# Устанавливаем WireGuard
+echo -e "\033[32m[INFO] Устанавливаю WireGuard...\033[0m"
 opkg update
 opkg install wireguard
 
-if [ $? -eq 0 ]; then
-    log_green "WireGuard успешно установлен."
-else
-    log_green "Ошибка установки WireGuard!"
+# Проверка установки WireGuard
+if ! command -v wg &> /dev/null
+then
+    echo -e "\033[31m[ERROR] WireGuard не установлен. Завершаю выполнение скрипта.\033[0m"
     exit 1
 fi
 
-# 2. Настройка конфигурации
-log_green "Настройка конфигурации WireGuard..."
+# Создаем конфигурацию для интерфейса
+WG_CONF_PATH="/etc/config/wireguard"
+echo -e "\033[32m[INFO] Создаю конфигурацию для интерфейса...\033[0m"
 
-# Путь к файлу конфигурации
-WG_CONFIG="/etc/config/network"
-
-# Сохранение конфигурации из переменной в файл
-cat <<EOF > $WG_CONFIG
+cat <<EOL > $WG_CONF_PATH
 config interface 'wg0'
     option proto 'wireguard'
     option private_key 'wKb8VjRNAcoECcFVlE89ZGh1YXgJ1FzPEB9Fz+QjGGI='
@@ -38,38 +30,25 @@ config wireguard_wg0
     option endpoint_host 'owrtrouters.kv9.ru'
     option endpoint_port '27988'
     option preshared_key 'vTdc1+h1IDUoP++dTDo+7BaL4HwlMLzgbnfK6rCdT9A='
-EOF
+    option persistent_keepalive '25'
+EOL
 
-# 3. Обновление маршрутов
-log_green "Обновление маршрутов..."
+# Перезагружаем сетевые интерфейсы
+echo -e "\033[32m[INFO] Перезагружаю сетевые интерфейсы...\033[0m"
+/etc/init.d/network restart
 
-# Добавляем статические маршруты для подключения
-uci set network.wg0.route='0.0.0.0/0'
-uci set network.wg0.route6='::/0'
-uci commit network
+# Настроим автозапуск WireGuard
+echo -e "\033[32m[INFO] Включаю автозапуск WireGuard...\033[0m"
+/etc/init.d/network enable
 
-# 4. Настройка автозапуска
-log_green "Настройка автозапуска WireGuard..."
-
-uci set system.@system[0].reboot='1'
-uci commit system
-
-# 5. Включение интерфейса WireGuard
-log_green "Включение интерфейса WireGuard..."
-
-ifup wg0
-
-if [ $? -eq 0 ]; then
-    log_green "WireGuard интерфейс успешно включен."
-else
-    log_green "Ошибка при включении интерфейса WireGuard!"
-    exit 1
-fi
-
-# 6. Проверка подключения
-log_green "Проверка состояния соединения WireGuard..."
-
+# Проверка статуса подключения WireGuard
+echo -e "\033[32m[INFO] Проверяю статус соединения...\033[0m"
 wg show wg0
 
-log_green "Настройка завершена. WireGuard настроен и подключен!"
-exit 0
+# Логирование успешной установки
+echo -e "\033[32m[INFO] WireGuard успешно установлен и настроен.\033[0m"
+echo -e "\033[32m[INFO] Соединение будет постоянно включено.\033[0m"
+
+# Выводим информацию о сетевых интерфейсах
+echo -e "\033[32m[INFO] Просмотр сетевых интерфейсов...\033[0m"
+ip addr show
